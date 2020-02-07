a5ed70a6372736a371eafc2148cec37d159c688d
Madhusudan <madhusudan70@gmail.com>
2020-02-07 11:03:37 +0530

    First Commit

---
+++ precache-manifest.87b75d68345f73fc86f7a12baf3e5e97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c30a86ae60f0da5b0f8a9e4789be05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bb4ef649078a3552ac9",
+    "url": "/static/css/2.1cad6180.chunk.css"
+  },
+  {
+    "revision": "825593d2c0d11371fede",
+    "url": "/static/css/main.912f9d60.chunk.css"
+  },
+  {
+    "revision": "8bb4ef649078a3552ac9",
+    "url": "/static/js/2.6ff010ba.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.6ff010ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "825593d2c0d11371fede",
+    "url": "/static/js/main.542b89be.chunk.js"
+  },
+  {
+    "revision": "1f72fe25347aa650a74d",
+    "url": "/static/js/runtime-main.12b8da2e.js"
+  }
+]);
